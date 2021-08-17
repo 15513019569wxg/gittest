aeb877f74950be2f5ccede019176b80b2210809d
15513019569wxg <15513019569@163.com>
2021-08-18 00:04:18 +0800

    first

---
+++ src/main/java/gittest/Test.java
@@ -0,0 +1,10 @@
+package gittest;/*
+    @author wxg
+    @date 2021/8/18-0:02
+    */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
